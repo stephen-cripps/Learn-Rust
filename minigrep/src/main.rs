56343f65b0928755507c2ac5cd00d137c321cb83
stephen-cripps <stephen.cripps@tutanota.com>
2023-09-05 16:44:58 +0100

    Finishing Minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("pProblem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application Error: {e}");
+        eprintln!("Application Error: {e}");
         process::exit(1);
     };
 }
